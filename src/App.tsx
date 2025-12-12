7cea60fab53e3914acc18fa3660cbd2d8f990f3f
wendeeeler <wendeeeler@gmail.com>
2025-12-11 21:19:46 -0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import Standards from './pages/Standards';
 import WarmingVsBurning from './pages/WarmingVsBurning';
@@ -7,13 +8,16 @@ import WarmingVsBurning from './pages/WarmingVsBurning';
 function App() {
   return (
     <Router>
-      <div className="min-h-screen">
+      <div className="flex flex-col min-h-screen">
         <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/standards" element={<Standards />} />
-          <Route path="/warming-vs-burning" element={<WarmingVsBurning />} />
-        </Routes>
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/standards" element={<Standards />} />
+            <Route path="/warming-vs-burning" element={<WarmingVsBurning />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
